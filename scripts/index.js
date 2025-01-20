eacc79d35bed919b8f76edd5eb029afd8660890b
Gregory Reshetnik <unormal322@yandex.ru>
2025-01-20 23:08:05 +0800

    second attempt

--- scripts/index.js
+++ scripts/index.js
@@ -5,24 +5,26 @@ const cardTemplate = document.querySelector('#card-template').content;
 const cardList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-function createCard(nameValue, linkValue) {
+function createCard(nameValue, linkValue, removeCard) {
     // Клонируем карточку
     const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
 
     // Добавляем наполнение карточки
     cardElement.querySelector('.card__title').textContent = nameValue;
     cardElement.querySelector('.card__image').src = linkValue;
+    cardElement.setAttribute('alt', nameValue);
 
     // Добавляем обработчик на кнопку
     const deleteButton = cardElement.querySelector('.card__delete-button');
-    deleteButton.addEventListener('click', () => deleteCard(cardElement));
+    deleteButton.addEventListener('click', removeCard);
 
     return cardElement;
 }
 
 // @todo: Функция удаления карточки
 function deleteCard(cardToRemove) {
-    cardToRemove.remove();
+    const item = cardToRemove.target.parentElement;
+    item.remove();
 }
 
 // @todo: Вывести карточки на страницу
